d4c1251230e9b99c7250dc536a971ffdec3be309
Harshita284 <Harshita.sh2202@gmail.com>
2025-07-17 11:17:43 +0530

    client and admin profile by id/harshita

--- backend/routes/client.router.js
+++ backend/routes/client.router.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { registerClientByAdmin, loginClient, getProfile, logoutClient, updateClientByAdmin, getAllClientByAdmin, resetPassword, forgotPassword , changePassword } = require('../controller/client.controller');
+const { registerClientByAdmin, loginClient, getProfile, logoutClient, updateClientByAdmin, getAllClientByAdmin, resetPassword, forgotPassword , changePassword, getClientById } = require('../controller/client.controller');
 const  upload  = require('../middleware/multer');
 const adminauthMiddleware = require('../middleware/authmiddleware')
 const authMiddleware = require('../middleware/allAuthmiddleware')
 const clientauthMiddleware = require('../middleware/clientAuthmiddleware')
 
 
-router.post('/add',adminauthMiddleware, authMiddleware, upload.single('image'), registerClientByAdmin)
+router.post('/add',adminauthMiddleware, authMiddleware, upload.single('profile'), registerClientByAdmin)
 router.post('/login', loginClient )
 router.get('/profile',clientauthMiddleware , getProfile)
 router.get('/logout', clientauthMiddleware, logoutClient)
@@ -16,5 +16,6 @@ router.put('/update/:id',adminauthMiddleware,authMiddleware,upload.single('profi
 router.post('/forgot-password', forgotPassword );
 router.post('/password', changePassword)
 router.put('/reset-password',clientauthMiddleware, resetPassword)
+router.get('/:id',adminauthMiddleware,getClientById)
 
 module.exports = router;
