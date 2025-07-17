16eab5e6a9ec3a6ae9bc0c3fd5d6023789bc3777
Harshita284 <Harshita.sh2202@gmail.com>
2025-07-17 13:48:20 +0530

    harshita

--- backend/app.js
+++ backend/app.js
@@ -23,10 +23,11 @@ app.get("/", (req, res) => {
 
 const adminRoute = require('./routes/admin.router');
 const clientRoute = require('./routes/client.router');
+const contactRoute = require('./routes/contact.router');
 
 // console.log(userRoute)
 
 app.use("/api/v1/admin", adminRoute);
 app.use("/api/v1/client", clientRoute);
-
+app.use("/api/v1/contact", contactRoute);
 module.exports = app;
--- backend/routes/admin.router.js
+++ backend/routes/admin.router.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { createAdmin, registerStaffByAdmin, login, profiledData, updateStaffByAdmin, logout, updateAdminSelf, resetEmailToken, changePassword, resetPassword, getAllStaff, getStaffById } = require('../controller/admin.controller');
+const { createAdmin, registerStaffByAdmin, login, profiledData, updateStaffByAdmin, logout, updateMyProfile, resetEmailToken, changePassword, resetPassword, getAllStaff, getStaffById } = require('../controller/admin.controller');
 const  upload  = require('../middleware/multer');
 const adminauthMiddleware = require('../middleware/authmiddleware')
 const authMiddleware = require('../middleware/allAuthmiddleware')
@@ -13,11 +13,10 @@ router.get('/profile', authMiddleware, profiledData)
 router.get('/logout', authMiddleware, logout)
 router.get("/staffs", adminauthMiddleware, authMiddleware, getAllStaff);
 router.put('/update/:id',adminauthMiddleware,upload.single('profile'), updateStaffByAdmin)
-router.put('/user', authMiddleware,upload.single('profile'), updateAdminSelf)
+router.put('/user', authMiddleware,upload.single('profile'), updateMyProfile)
 router.post('/forgot-password', resetEmailToken);
 router.post('/password', changePassword)
 router.put('/reset-password',authMiddleware, resetPassword)
-
 router.get("/:id", adminauthMiddleware,authMiddleware, getStaffById);
 
 module.exports = router;
