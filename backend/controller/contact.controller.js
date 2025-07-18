d271fdd02c836d9cdadda9a19b743783b755910d
Harshita284 <Harshita.sh2202@gmail.com>
2025-07-18 10:46:29 +0530

    harshita contact

--- backend/controller/contact.controller.js
+++ backend/controller/contact.controller.js
@@ -2,30 +2,86 @@ const ContactModel = require('../model/contact.model');
 
 
 exports.addContact = async (req, res) => {
-      try{
-            const { name, email,phone, subject, message } = req.body;
+  try {
+    const { name, email, phone, subject, message } = req.body;
 
-            if (!name || !email || !subject || !message) {
-                  return res.status(400).json({ message: "All fields are required" });
+
+    if (!name || !email || !phone || !subject || !message) {
+      return res.status(400).json({ success: false, message: "All fields are required." });
+    }
+
+    const contact = await ContactModel.create({
+      name,
+      email,
+      phone,
+      subject,
+      message,
+    });
+
+    res.status(201).json({
+      success: true,
+      message: "Contact form submitted successfully!",
+      data: contact,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Something went wrong while submitting contact form.",
+      error: error.message,
+    });
+  }
+};
+
+
+exports.getAllContact = async (req, res) => {
+      try{
+            const userRole = req.user.role;
+
+            if(!["admin"].includes(userRole)){
+                  return res.status(401).json({message: "Only admin can get all contact data."})
             }
 
-            const newContact = new ContactModel({
-                  name,
-                  email,
-                  phone,
-                  subject,
-                  message
-            });
+            const contact = await ContactModel.find();
 
-            await newContact.save();
-            return res.status(201).json({
-                  message: "Contact message sent successfully",
-                  contact: newContact
-            });
+            if(!contact){
+                  return res.status(400).json({message: "Can not found contact."})
+            }
+
+            res.status(200).json({
+                  success: true,
+                  message: "Fetch All Data Successfully.",
+                  contact
+            })
 
       }catch(error){
-            console.error(error);
-            return res.status(500).json({ message: "Server error" });
+            return res.status(500).json({message: "Server Error.", error: error.message})
       }
+}
 
+exports.getbyId = async (req, res) => {
+      try{
+            const {id} = req.params;
+            const userRole = req.user.role;
+
+             if(!["admin"].includes(userRole)){
+                  return res.status(401).json({message: "Only admin can get contact data."})
+            }
+
+            const contact = await ContactModel.findById(id);
+
+             if(!contact){
+                  return res.status(400).json({message: "Can not found contact."})
+            }
+
+              res.status(200).json({
+                  success: true,
+                  message: "Fetch Data Successfully.",
+                  contact
+            })
+
+
+      }catch(error){
+            return res.status(500).json({message: "Server Error.", error: error.message})
+
+      }
 }
