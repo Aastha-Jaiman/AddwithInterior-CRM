688a3c85bbefd5b74f7f5909d42d435af34f32df
Harshita284 <Harshita.sh2202@gmail.com>
2025-09-12 14:40:46 +0530

    harshita/ fix error

--- backend/model/paymentHistory.model.js
+++ backend/model/paymentHistory.model.js
@@ -11,6 +11,7 @@ const PaymentHistorySchema = new mongoose.Schema(
       type: mongoose.Schema.Types.ObjectId,
       ref: "Project",
       required: [true, "Project reference is required"],
+      unique: true,
     },
 
     totalPrice: {
