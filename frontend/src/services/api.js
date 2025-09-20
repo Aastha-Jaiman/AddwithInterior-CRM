3c201390bfcf4f004c67481fafaf18d04198a33c
Aastha-Jaiman <aasthajaiman2005@gmail.com>
2025-09-20 12:11:10 +0530

    url /aastha

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api/v1/",
-  // baseURL: "https://addwithinterior-crm.onrender.com/api/v1/",
+  // baseURL: "http://localhost:5000/api/v1/",
+  baseURL: "https://addwithinterior-crm.onrender.com/api/v1/",
   withCredentials: true,
 });
 
