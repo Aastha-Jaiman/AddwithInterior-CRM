c54062d4daedc98a7b3744a73fe10fbd7b3aa9de
Aastha-Jaiman <aasthajaiman2005@gmail.com>
2025-10-06 17:00:52 +0530

    deploy/aastha

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:3000" , "https://addwith-interior-crm.vercel.app/"
+    origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:3000" , "https://addwith-interior-crm.vercel.app"
 ],
     credentials: true,
   })
